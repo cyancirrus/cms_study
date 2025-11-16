48beb215ccbbf544454365cc176e904e00838160
autumn <cyancirrus@gmail.com>
2025-11-16 00:14:12 -0600

    yay can stand up end point for api tomorrow easily and like, model is already outperforming, etl is somewhat coming along

--- scripts/requests.sh
+++ scripts/requests.sh
@@ -3,17 +3,14 @@ source ./scripts/launch_api.sh
 source ./scripts/terminate_api.sh
 
 retrieve_recommended_hospital() {
-    base_url="http://127.0.0.1:5678/recommend_hospital?"
-    params="zip_code=12345&"
-    params+="hospital_type=womens&"
-    params+="service_category=psychiatric"
+	base_url="http://127.0.0.1:5678/recommend_hospital?"
+	params="zip_code=12345&"
+	params+="hospital_type=womens&"
+	params+="service_category=psychiatric"
 
-    curl -s "${base_url}${params}"
+	curl -s "${base_url}${params}"
 }
-
 
 launch
 retrieve_recommended_hospital
 shutdown
-
-
