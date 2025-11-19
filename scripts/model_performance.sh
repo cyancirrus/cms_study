a4ac690a4c9c6f9d2087edd97d81576f45657dfe
autumn <cyancirrus@gmail.com>
2025-11-19 13:44:13 -0600

    looking good

--- scripts/model_performance.sh
+++ scripts/model_performance.sh
@@ -2,14 +2,14 @@
 source ./scripts/environment.sh
 
 model_explore() {
-	echo "-------------------------------------"
-	echo "             Medicare                "
-	echo "-------------------------------------"
-	python ./src/model_medicare_main.py
-	echo "-------------------------------------"
-	echo "             Psychiatric             "
-	echo "-------------------------------------"
-	python ./src/model_psychiatric_main.py
+	# echo "-------------------------------------"
+	# echo "             Medicare                "
+	# echo "-------------------------------------"
+	# python ./src/model_medicare_main.py
+	# echo "-------------------------------------"
+	# echo "             Psychiatric             "
+	# echo "-------------------------------------"
+	# python ./src/model_psychiatric_main.py
 	echo "-------------------------------------"
 	echo "             General                 "
 	echo "-------------------------------------"
@@ -47,4 +47,4 @@ model_checkin() {
 }
 
 model_explore
-model_checkin
+# model_checkin
